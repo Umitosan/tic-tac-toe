0001363a2cfd0dda01a97e2006fa528f017201e8
Dominic Brown and Kat Gifford <kat@>
2017-04-05 15:44:07 -0700

    add functionality to pick piece and logic to toggle pieces

--- js/scripts.js
+++ js/scripts.js
@@ -18,23 +18,18 @@ Tile.prototype.changeColor = function() {
 
 }
 
-// function
-// board
-//   tiles
-//   tiles states
-//   tile colors
-//   function() create board
-//   function() update tile placement
-//   function() check for legal placement
-//   function() check for win
 
-// player
-//   pieces X or O
-//   wins
-//
-// game
-//   score
-//   player turn
+var placePiece = function(myToggle) {
+  let playerPiece = "";
+  if (myToggle === 1) {
+    playerPiece = "X";
+  } else if (myToggle === -1) {
+    playerPiece = "O";
+  } else {
+    console.log("somehting wrong")
+  }
+  return playerPiece;
+}
 
 
 //Front End//
@@ -42,17 +37,17 @@ $(document).ready(function() {
 
    // Create first board
    var newBoard = new Board();
+   var pieceToggle = 1;
 
-  // $("#newGame").click(function() {
-  //   var newBoard = new Board();
-  //   $(".tileCol").show();
-  // });
 
+  // tile select indicator
   $("div .tile").click(function() {
+
+    pieceToggle *= -1;
+    $(this).find("span").text(placePiece(pieceToggle));
 
     $(".tileCol .tile").removeClass("green-back");
     $(".tileCol .tile").removeClass("white-back");
-
     $(this).addClass("green-back");
   });
 
