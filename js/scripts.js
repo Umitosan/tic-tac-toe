b5f5e362cc0fd14205168971db93fe0be95327ca
Dominic Brown and Kat Gifford <dominic@>
2017-04-05 14:50:17 -0700

    html and css changes to style board

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,55 @@
 //Back End//
 
-//constructors
+// GAME BOARD
+function Board() {
+  this.tilesArr = [];
+}
+Board.prototype.createBoard = function() {
 
-//prototypes
+}
+Board.prototype.updateBoard = function() {
+
+}
+
+function Tile() {
+  this.color = color;
+}
+Tile.prototype.changeColor = function() {
+
+}
+
+// function
+// board
+//   tiles
+//   tiles states
+//   tile colors
+//   function() create board
+//   function() update tile placement
+//   function() check for legal placement
+//   function() check for win
+
+// player
+//   pieces X or O
+//   wins
+//
+// game
+//   score
+//   player turn
 
 
 //Front End//
 $(document).ready(function() {
 
-  $("").click(function(){
+   // Create first board
+   var newBoard = new Board();
+
+  // $("#newGame").click(function() {
+  //   var newBoard = new Board();
+  //   $(".tileCol").show();
+  // });
+
+  $("boardArea").click(function() {
+    // create board
   });
 
 });
