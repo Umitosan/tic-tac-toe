1d2bc7845054940fc26388025d3ef8641e73962b
Dominic Brown and Kat Gifford <kat@>
2017-04-06 11:57:49 -0700

    add prototype to check rows for winner, test and works

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,9 @@
-//Back End//
+//Back End
+
+// TILE OBJECT
+function Tile() {
+  this.piece = "";
+}
 
 // GAME BOARD OBJECT
 function Board() {
@@ -6,6 +11,49 @@ function Board() {
   this.playerTurn = -1;
 }
 
+Board.prototype.winnerChickenDinner = function() {
+  checkHoriz();
+  // checkVert();
+  // checkDiagonAlley();
+}
+
+// check rows for win, works for X and O
+Board.prototype.checkHoriz = function(tmpIndex) {
+  var tmpPiece = this.tilesArr[tmpIndex].piece;
+  if ((this.tilesArr[0].piece === tmpPiece) && (this.tilesArr[1].piece === tmpPiece) && (this.tilesArr[2].piece === tmpPiece)) {
+    console.log("winner");
+  } else if ((this.tilesArr[3].piece === tmpPiece) && (this.tilesArr[4].piece === tmpPiece) && (this.tilesArr[5].piece === tmpPiece)) {
+    console.log("winner");
+  } else if ((this.tilesArr[6].piece === tmpPiece) && (this.tilesArr[7].piece === tmpPiece) && (this.tilesArr[8].piece === tmpPiece)) {
+    console.log("winner");
+  } else {
+    console.log("soy un perdedor");
+  }
+}
+
+// Board.prototype.checkVert = function () {
+//   if 0 3 6 {
+//
+//   } else if 1 4 7 {
+//
+//   } else if 2 5 8 {
+//
+//   } else {
+//     console.log("no winner yet");
+//   }
+// }
+//
+// Board.prototype.checkDiagonAlley = function () {
+//   if 0 4 8 {
+//
+//   } else if 6 4 2 {
+//
+//   } else {
+//     console.log("no winner yet");
+//   }
+// }
+
+// create board object and fill it with tile objects
 var createBoard = function() {
   var newBoard = new Board();
     for (i = 0 ; i < 9 ; i++) {
@@ -16,11 +64,8 @@ var createBoard = function() {
   return newBoard;
 }
 
-// TILE OBJECT
-function Tile() {
-  this.piece = "";
-}
 
+// isMoveLegal? place piece on board, and store piece in tile object
 var placePiece = function(tmpBoard , clickedIndex) {
   let playerPiece = "";
   if (isMoveLegal(tmpBoard.tilesArr[clickedIndex]) === true) {
@@ -40,9 +85,9 @@ var placePiece = function(tmpBoard , clickedIndex) {
   }
   console.log("turn: " , tmpBoard.playerTurn);
   return playerPiece;
-} // placePiece function end
+} // END placePiece function
 
-
+// check if a move is legal
 var isMoveLegal = function(tmpTile) {
   var tmpBool;
   if (tmpTile.piece === "") {
@@ -52,26 +97,22 @@ var isMoveLegal = function(tmpTile) {
   }
   console.log("tmpBool: " , tmpBool);
   return tmpBool;
-}
+}  // END isMoveLegal function
 
 
 //Front End//
 $(document).ready(function() {
-
-   // Create first board
-   var myBoard = createBoard();
-
+  // Create first board
+  var myBoard = createBoard();
   // tile select indicator
   $("div .tile").click(function() {
-
     let thisTileIndex = $(this).attr("value");
-
     $(this).find("span").text(placePiece(myBoard , thisTileIndex));
-
+    // console.log("Check horiz win? " ,myBoard);
+    myBoard.checkHoriz(thisTileIndex);
     // indicate selected tile
     $(".tileCol .tile").removeClass("green-back");
     $(".tileCol .tile").removeClass("white-back");
     $(this).addClass("green-back");
   });
-
 });
